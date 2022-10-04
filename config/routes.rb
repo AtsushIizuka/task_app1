70db8f633fd42d30b839d581af31e04c62222d06
AtsushIizuka <112635976+AtsushIizuka@users.noreply.github.com>
2022-10-04 16:58:14 +0900

    修正

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
-  get 'posts/index'
+  get 'posts/index', to: 'posts#index'
+  get 'posts/new',to:'posts#new'
+  get 'posts/show',to:'posts#show'
+  get 'posts/edit',to:'posts#edit'
   
   
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
